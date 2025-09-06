florianboergel/pyTEF

commit 619e4a158d118c392a6b63b68280cc5293bca9f3

Increase performance of 2D TEF and mask empty bins
By introducing a simple if-condition in the double for-loop of the
sort_2dim function, the computation can be made much faster.  In the
2D-TEF diagram (e.g., with temperature and salinity), there are often
many empty bins with no data for a given T-S-combination.  By skipping
these bins, we save a lot of computations and thus time.  In test runs,
the computation time decreased by more than 50 %.  Furthermore, bins
that are empty at all time steps are now marked by NaN (instead of 0),
so that the 2D-TEF diagram shows the difference between bins that are
always empty and those with zero transport.

01_calc.ipynb
@@ -425,10 +425,12 @@
     "        \n",
     "        for i in tqdm(range(N1)):\n",
     "            for j in range(N2):\n",
-    "                #print(self._get_name_depth(),self._get_name_latitude(),self._get_name_longitude())\n",
-    "                out_q[:, i, j] = constructorTEF.transport.where((idx == i) & (idy == j)).sum([\"depth\",\n",
-    "                                                                                              \"lat\",\n",
-    "                                                                                              \"lon\"],dtype=np.float64) / delta_var / delta_var2\n",
+    "                indices = (idx == i) & (idy == j)\n",
+    "                if indices.any():\n",
+    "                    out_q[:, i, j] = constructorTEF.transport.where(indices).sum([\"depth\", \"lat\", \"lon\"],\n",
+    "                                                                                 dtype=np.float64) / delta_var / delta_var2\n",
+    "                else:\n",
+    "                    out_q[:, i, j] = np.NaN\n",
     "        \n",
     "        out_Q = np.zeros((len(constructorTEF.ds.time), N1+1, N2+1))\n",
     "        out_Q_tmp = np.cumsum(np.cumsum(out_q[:,::-1,::-1],axis=1),axis=2)[:,::-1,::-1]*delta_var2*delta_var\n",

pyTEF/calc.py
@@ -276,10 +276,12 @@ def sort_2dim(constructorTEF,
         
         for i in tqdm(range(N1)):
             for j in range(N2):
-                #print(self._get_name_depth(),self._get_name_latitude(),self._get_name_longitude())
-                out_q[:, i, j] = constructorTEF.transport.where((idx == i) & (idy == j)).sum(["depth",
-                                                                                              "lat",
-                                                                                              "lon"],dtype=np.float64) / delta_var / delta_var2
+                indices = (idx == i) & (idy == j)
+                if indices.any():
+                    out_q[:, i, j] = constructorTEF.transport.where(indices).sum(["depth", "lat", "lon"],
+                                                                                 dtype=np.float64) / delta_var / delta_var2
+                else:
+                    out_q[:, i, j] = np.NaN
         
         out_Q = np.zeros((len(constructorTEF.ds.time), N1+1, N2+1))
         out_Q_tmp = np.cumsum(np.cumsum(out_q[:,::-1,::-1],axis=1),axis=2)[:,::-1,::-1]*delta_var2*delta_var

commit 0d90cfbffda2b0681da5a4dede6806ca5dfacfc0

Fix NaN-values in 2-dimensional TEF binning
An error introduced in commit 619e4a1 is corrected:
The 2-dimensional TEF streamfunction Q could contain a lot of NaN values
if there were NaNs in q, but actually, Q should not contain any NaNs.

Also, NaN is replaced by nan for compatibility with NumPy version 2.

01_calc.ipynb
@@ -430,10 +430,10 @@
     "                    out_q[:, i, j] = constructorTEF.transport.where(indices).sum([\"depth\", \"lat\", \"lon\"],\n",
     "                                                                                 dtype=np.float64) / delta_var / delta_var2\n",
     "                else:\n",
-    "                    out_q[:, i, j] = np.NaN\n",
+    "                    out_q[:, i, j] = np.nan\n",
     "        \n",
     "        out_Q = np.zeros((len(constructorTEF.ds.time), N1+1, N2+1))\n",
-    "        out_Q_tmp = np.cumsum(np.cumsum(out_q[:,::-1,::-1],axis=1),axis=2)[:,::-1,::-1]*delta_var2*delta_var\n",
+    "        out_Q_tmp = np.nancumsum(np.nancumsum(out_q[:,::-1,::-1],axis=1),axis=2)[:,::-1,::-1]*delta_var2*delta_var\n",
     "        out_Q[:,:-1,:-1] = out_Q_tmp\n",
     "        \n",
     "        out = xr.Dataset({\n",

pyTEF/calc.py
@@ -281,10 +281,10 @@ def sort_2dim(constructorTEF,
                     out_q[:, i, j] = constructorTEF.transport.where(indices).sum(["depth", "lat", "lon"],
                                                                                  dtype=np.float64) / delta_var / delta_var2
                 else:
-                    out_q[:, i, j] = np.NaN
+                    out_q[:, i, j] = np.nan
         
         out_Q = np.zeros((len(constructorTEF.ds.time), N1+1, N2+1))
-        out_Q_tmp = np.cumsum(np.cumsum(out_q[:,::-1,::-1],axis=1),axis=2)[:,::-1,::-1]*delta_var2*delta_var
+        out_Q_tmp = np.nancumsum(np.nancumsum(out_q[:,::-1,::-1],axis=1),axis=2)[:,::-1,::-1]*delta_var2*delta_var
         out_Q[:,:-1,:-1] = out_Q_tmp
         
         out = xr.Dataset({